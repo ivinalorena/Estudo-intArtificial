13eef035b9ce390338c7f57a6529b46308eab05f
Ivina Lorena Oliveira Moura <ivinalorena@aluno.unilab.edu.br>
2025-10-13 16:08:22 -0300

    Lambda functions

--- freecampcode/Scientific Computing with Python/Lambdafunctions.py
+++ freecampcode/Scientific Computing with Python/Lambdafunctions.py
@@ -2,4 +2,23 @@
 As funções Lambda oferecem uma maneira concisa de escrever funções pequenas e descartáveis ​​em seu código.
 Neste projeto, o objetivo é explorar o poder das Funções Lambda criando um rastreador de despesas.
 O aplicativo resultante demonstrará como você pode usar as Funções Lambda para operações eficientes e simplificadas.
+
+Revisão de listas: 
+
+my_list = [1, 2]
+
+my_list.append(3)
+print(my_list)
+
+print(my_list[0])
+
+my_list[0] = 0
+print(my_list)
+
+my_list.insert(1, 1)
+print(my_list)
+
+my_list.pop()
+print(my_list)
 '''
+
