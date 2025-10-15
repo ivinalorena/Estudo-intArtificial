8eb5a4d981d3158c1474aebc5349a89d790b86cc
Ivina Lorena Oliveira Moura <ivinalorena@aluno.unilab.edu.br>
2025-10-15 17:24:35 -0300

    mais estudos sobre sql-basics

--- SQL-Fundamentals/sqlbasics.sql
+++ SQL-Fundamentals/sqlbasics.sql
@@ -71,3 +71,52 @@ WHERE day = 'Friday';
 select price, condition
 from device_offer
 where os ='iOS';
+
+--Checking a list The word IN allows us to check if an item is in a list. 
+--The example shows the name and population for the countries 'Brazil', 'Russia', 'India' and 'China'.
+--Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+SELECT name, population FROM world
+  WHERE name IN ('sweden','Norway','Denmark');
+
+--Which countries are not too small and not too big? BETWEEN allows range checking (range specified is inclusive of boundary values). 
+--The example below shows countries with an area of 250,000-300,000 sq. km. Modify it to show the country and the area for countries with an area between 200,000 and 250,000.
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+--Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
+select name from world
+where population >= 200000000;
+
+--Give the name and the per capita GDP for those countries with a population of at least 200 million.
+select name, gdp/population as per_capita_gdp
+from world
+where population >= 200000000;
+
+--Show the name and population in millions for the countries of the continent 'South America'. Divide the population by 
+--1000000 to get population in millions.
+select name, population/1000000
+from world
+where continent = 'South America';
+--Show the name and population for France, Germany, Italy
+select name, population
+from world
+where name IN ('France','Germany','Italy');
+--Show the countries which have a name that includes the word 'United'
+select name
+from world
+where name LIKE '%United%';
+
+/* Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+
+Show the countries that are big by area or big by population. Show name, population and area. */
+select name, population, area
+from world
+where area > 3000000 OR population > 250000000;
+
+/* Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
+Australia has a big area but a small population, it should be included.
+Indonesia has a big population but a small area, it should be included.
+China has a big population and big area, it should be excluded.
+United Kingdom has a small population and a small area, it should be excluded. */
+select name, population, area
+from world
+where population > 250000000 XOR area > 3000000;
