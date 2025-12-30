2b243d9749713c104d1baf2a0f4738b4862b7027
Ivina Lorena Oliveira Moura <108935037+ivinalorena@users.noreply.github.com>
2025-12-30 17:44:26 -0300

    curso sql teomewhy

---
+++ curso-teomewhy-yt/00.sql
@@ -0,0 +1,7 @@
+-- Observação: o curso é bem introdutório 
+
+-- aula 00: introdução
+-- Profissionais de dados:
+-- engenharia de dados, análise de dados, ciência de dados -> todos usam sql para armazenar, organizar e consultar os daods
+-- aula 01: primeiros conceitos
+
